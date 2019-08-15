f062e699493352f4e18bdc6b2f589d5b54c5d79d
Ryan Switzer <ryanaswitzer@gmail.com>
2019-08-15 18:15:09 -0400

    feat(builder): added builder to main (#1)

---
+++ EFthis/CodeGeneration/Shared.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFthis.CodeGeneration
+{
+    public static class Shared
+    {
+        public static readonly Dictionary<string, string> DataMap = new Dictionary<string, string>
+    {
+        { "bigint", "long" }
+        ,{ "bit", "bool" }
+        ,{ "char", "string" }
+        ,{ "nchar", "string" }
+        ,{ "date", "DateTime" }
+        ,{ "datetime", "DateTime" }
+        ,{ "datetime2", "DateTime" }
+        ,{ "decimal", "decimal" }
+        ,{ "float", "decimal" }
+        ,{ "hierarchyid", "NotSupported" }
+        ,{ "int", "int" }
+        ,{ "money", "decimal" }
+        ,{ "numeric", "decimal" }
+        ,{ "nvarchar", "string" }
+        ,{ "smallint", "short" }
+        ,{ "time", "TimeSpan" }
+        ,{ "uniqueidentifier", "Guid" }
+        ,{ "varbinary", "byte[]" }
+        ,{ "varchar", "string" }
+        ,{ "xml", "string" }
+    };
+
+        public static readonly HashSet<string> NonNullableMap = new HashSet<string>
+    {
+        "bigint"
+        , "bit"
+        , "date"
+        , "datetime"
+        , "datetime2"
+        , "decimal"
+        , "float"
+        , "int"
+        , "money"
+        , "numeric"
+        , "smallint"
+        , "time"
+        , "uniqueidentifier"
+    };
+
+        public static readonly HashSet<string> NullableMap = new HashSet<string>
+    {
+        "char"
+        , "nchar"
+        , "nvarchar"
+        , "varbinary"
+        , "varchar"
+        , "xml"
+    };
+
+
+        public static string NameSanitize(string name)
+        {
+            name = name.Replace("_", " ");
+            if (name.Contains(" "))
+            {
+                name = name.Split(new[] { ' ' })
+                .Aggregate(string.Empty, (acc, curr) =>
+                {
+                    if (IsAllUpper(curr))
+                    {
+                        curr = curr.ToLower();
+                    }
+
+                    if (char.IsLower(curr[0]))
+                    {
+                        curr = char.ToUpper(curr[0]) + curr.Remove(0, 1);
+                    }
+
+                    return $"{acc}{curr}";
+                });
+            }
+
+            if (IsAllUpper(name))
+            {
+                name = name.ToLower();
+                name = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+            }
+
+            var upperRun = new List<char>();
+            for (int i = 0; i < name.Length; i++)
+            {
+                var character = name[i];
+                if (char.IsUpper(character))
+                {
+                    upperRun.Add(character);
+                    continue;
+                }
+
+                if (upperRun.Count < 3)
+                {
+                    upperRun.Clear();
+                    continue;
+                }
+
+                var newString = new string(upperRun.ToArray()).ToLower();
+                newString = char.ToUpper(newString[0]) + newString.Remove(0, 1);
+                newString = newString.Remove(newString.Length - 1, 1) + char.ToUpper(newString[newString.Length - 1]);
+                name = name.Replace(new string(upperRun.ToArray()), newString);
+                upperRun.Clear();
+            }
+
+            if (upperRun.Count > 1)
+            {
+                var newString = new string(upperRun.ToArray()).ToLower();
+                newString = char.ToUpper(newString[0]) + newString.Remove(0, 1);
+                name = name.Replace(new string(upperRun.ToArray()), newString);
+            }
+
+            return name;
+        }
+
+        private static bool IsAllUpper(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]) && !Char.IsUpper(input[i]))
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ EFthis/CodeGeneration/TableMetadataBuilder.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace EFthis.CodeGeneration
+{
+    public class TableMetaDataBuilder
+    {
+        private readonly IDbConnection _connection;
+
+        public TableMetaDataBuilder(IDbConnection connection)
+        {
+            _connection = connection;
+
+            if (_connection.State != ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+        }
+
+        public List<TableProperty> GetProperties(string table, string schema)
+        {
+            const string sql = @" -- get columns query
+				SELECT
+					c.COLUMN_NAME AS ColumnName
+					, c.ORDINAL_POSITION AS Position
+					, c.IS_NULLABLE AS IsNullable
+					, c.DATA_TYPE AS DataType
+					, c.CHARACTER_MAXIMUM_LENGTH AS Size
+					, c.NUMERIC_PRECISION AS Precision
+					, c.NUMERIC_SCALE AS Scale
+				FROM    INFORMATION_SCHEMA.COLUMNS c
+				WHERE   c.TABLE_NAME = @TableName and ISNULL(@TableSchema, c.TABLE_SCHEMA) = c.TABLE_SCHEMA  
+				ORDER BY c.ORDINAL_POSITION";
+
+            var pks = GetPk(table, schema);
+            var computeds = GetComputed(table, schema);
+            var identities = GetIdentity(table, schema);
+            var fks = GetFks(table, schema);
+            var dependentTables = GetDependentTables(table, schema);
+
+            var properties = new List<TableProperty>();
+            using (var cmd = new SqlCommand(sql, (SqlConnection)_connection))
+            {
+                cmd.Parameters.Add(new SqlParameter("@TableName", table));
+                cmd.Parameters.Add(new SqlParameter("@TableSchema", schema));
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var size = default(int?);
+                        if (int.TryParse(reader["Size"].ToString(), out var tmpSize))
+                        {
+                            size = tmpSize;
+                        }
+
+                        var precision = default(int?);
+                        if (int.TryParse(reader["Precision"].ToString(), out var tmpPrecision))
+                        {
+                            precision = tmpPrecision;
+                        }
+
+                        var scale = default(int?);
+                        if (int.TryParse(reader["Scale"].ToString(), out var tmpScale))
+                        {
+                            scale = tmpScale;
+                        }
+
+                        var colName = reader["ColumnName"].ToString();
+
+                        var prop = new TableProperty
+                        {
+                            ColumnName = colName,
+                            Position = int.Parse(reader["Position"].ToString()),
+                            DataType = reader["DataType"].ToString(),
+                            PrimaryTable = fks
+                                .Where(x => x.column == colName)
+                                .Select(x => x.table)
+                                .FirstOrDefault(),
+                            DependentTables = dependentTables
+                                .Where(x => x.refColumn == colName)
+                                .Select(x => x.dependentTable)
+                                .ToList(),
+                            IsNullable = reader["IsNullable"].ToString() == "YES",
+                            Size = size,
+                            Precision = precision,
+                            Scale = scale,
+                            IsComputed = computeds.Contains(colName),
+                            IsIdentity = identities.Contains(colName),
+                            IsKey = pks.Contains(colName),
+                        };
+
+                        properties.Add(prop);
+                    }
+                }
+            }
+
+            return properties;
+        }
+
+        private List<string> GetPk(string table, string schema)
+        {
+            const string sql = @" -- PK columns
+				SELECT ku.COLUMN_NAME AS Name
+				FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS tc
+				INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS ku
+				    ON tc.CONSTRAINT_TYPE = 'PRIMARY KEY' 
+				    AND tc.CONSTRAINT_NAME = ku.CONSTRAINT_NAME
+					AND tc.CONSTRAINT_SCHEMA = ku.CONSTRAINT_SCHEMA
+					AND tc.TABLE_CATALOG = ku.TABLE_CATALOG
+					AND tc.TABLE_NAME = ku.TABLE_NAME
+				WHERE ku.TABLE_NAME = @TableName and ISNULL(@TableSchema, ku.TABLE_SCHEMA) = ku.TABLE_SCHEMA";
+
+            var properties = new List<string>();
+            using (var cmd = new SqlCommand(sql, (SqlConnection)_connection))
+            {
+                cmd.Parameters.Add(new SqlParameter("@TableName", table));
+                cmd.Parameters.Add(new SqlParameter("@TableSchema", schema));
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        properties.Add(reader["Name"].ToString());
+                    }
+                }
+            }
+
+            return properties;
+        }
+
+        private List<string> GetIdentity(string table, string schema)
+        {
+            const string sql = @" -- identity columns
+				select COLUMN_NAME AS Name
+				from INFORMATION_SCHEMA.COLUMNS
+				where COLUMNPROPERTY(object_id('AspNetUsers'), COLUMN_NAME, 'IsIdentity') = 1
+				AND TABLE_NAME = @TableName and ISNULL(@TableSchema, TABLE_SCHEMA) = TABLE_SCHEMA";
+
+            var properties = new List<string>();
+            using (var cmd = new SqlCommand(sql, (SqlConnection)_connection))
+            {
+                cmd.Parameters.Add(new SqlParameter("@TableName", table));
+                cmd.Parameters.Add(new SqlParameter("@TableSchema", schema));
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        properties.Add(reader["Name"].ToString());
+                    }
+                }
+            }
+
+            return properties;
+        }
+
+        private List<string> GetComputed(string table, string schema)
+        {
+            const string sql = @" -- computed columns
+				SELECT Name
+				FROM sys.computed_columns
+				WHERE object_id = OBJECT_ID(@TableSchema + '.' + @TableName)";
+
+            var properties = new List<string>();
+            using (var cmd = new SqlCommand(sql, (SqlConnection)_connection))
+            {
+                cmd.Parameters.Add(new SqlParameter("@TableName", table));
+                cmd.Parameters.Add(new SqlParameter("@TableSchema", schema));
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        properties.Add(reader["Name"].ToString());
+                    }
+                }
+            }
+
+            return properties;
+        }
+
+        private List<(string dependentTable, string refColumn)> GetDependentTables(string table, string schema)
+        {
+            const string sql = @" -- Dependent tables
+				SELECT
+				   OBJECT_NAME(f.parent_object_id) AS DependentTable,
+				   col.name AS ReferenceColumn
+				FROM 
+				   sys.foreign_keys AS f
+				INNER JOIN 
+				   sys.foreign_key_columns AS fc 
+				      ON f.OBJECT_ID = fc.constraint_object_id
+				INNER JOIN 
+				   sys.tables t 
+				      ON t.OBJECT_ID = f.referenced_object_id
+				INNER JOIN
+					sys.columns col
+					  ON col.object_id = t.object_id AND column_id = fc.referenced_column_id
+				WHERE 
+				   f.referenced_object_id = OBJECT_ID(@TableSchema + '.' + @TableName)";
+
+            var properties = new List<(string, string)>();
+            using (var cmd = new SqlCommand(sql, (SqlConnection)_connection))
+            {
+                cmd.Parameters.Add(new SqlParameter("@TableName", table));
+                cmd.Parameters.Add(new SqlParameter("@TableSchema", schema));
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        properties.Add((reader["DependentTable"].ToString(), reader["ReferenceColumn"].ToString()));
+                    }
+                }
+            }
+
+            return properties;
+        }
+
+        private List<(string column, string table)> GetFks(string table, string schema)
+        {
+            const string sql = @" -- fk columns
+				select 
+				    col.name as ColumnName,
+				    pk_tab.name as PrimaryTable
+				from sys.tables tab
+				    inner join sys.columns col 
+				        on col.object_id = tab.object_id
+				    left outer join sys.foreign_key_columns fk_cols
+				        on fk_cols.parent_object_id = tab.object_id
+				        and fk_cols.parent_column_id = col.column_id
+				    left outer join sys.foreign_keys fk
+				        on fk.object_id = fk_cols.constraint_object_id
+				    left outer join sys.tables pk_tab
+				        on pk_tab.object_id = fk_cols.referenced_object_id
+				    left outer join sys.columns pk_col
+				        on pk_col.column_id = fk_cols.referenced_column_id
+				        and pk_col.object_id = fk_cols.referenced_object_id
+				where fk.object_id is not null AND tab.name = @TableName
+					AND ISNULL(@TableSchema, schema_name(tab.schema_id)) = schema_name(tab.schema_id)
+				order by schema_name(tab.schema_id) + '.' + tab.name,
+				    col.column_id";
+
+            var properties = new List<(string, string)>();
+            using (var cmd = new SqlCommand(sql, (SqlConnection)_connection))
+            {
+                cmd.Parameters.Add(new SqlParameter("@TableName", table));
+                cmd.Parameters.Add(new SqlParameter("@TableSchema", schema));
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        properties.Add((reader["ColumnName"].ToString(), reader["PrimaryTable"].ToString()));
+                    }
+                }
+            }
+
+            return properties;
+        }
+    }
+}
